e28d345990d255b10096f4e38b93e1d6d4b170e4
tuliooassis <tulioassis@outlook.com>
2017-12-10 00:57:45 -0200

    alguma coisa

--- app/src/main/java/com/example/ana/volunteerwork/MainActivity.java
+++ app/src/main/java/com/example/ana/volunteerwork/MainActivity.java
@@ -166,13 +166,12 @@ public class MainActivity extends AppCompatActivity
             ft.commit();
 
             //FragmentTransaction ft = fragmentManager.beginTransaction().replace(R.id.frame_layout, new EventTransitionFragment()).commit();
-        } /* else if (id == R.id.nav_profile) {
-
-        } /*else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
+        }
+        else if (id == R.id.nav_logout){
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
